98e653b30a0de8312ddb24bf364b7d283ea02ade
Lilitalop <89038353+Lilitalop@users.noreply.github.com>
2026-02-21 14:02:12 -0500

    Add files via upload

---
+++ asignaci-n-2-openbootcamp/README.md
@@ -0,0 +1,6 @@
+# Asignación 2 OpenBootCamp
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Liliana-Yelitza-L-pez-de-Chaparro/pen/gbMJqJo](https://codepen.io/Liliana-Yelitza-L-pez-de-Chaparro/pen/gbMJqJo).
+
---
+++ asignaci-n-2-openbootcamp/dist/script.js
@@ -0,0 +1,29 @@
+//********************
+
+ // Initial code
+  let product = {name: 'cargador',
+  price: 280,
+  doesItWork: false,
+  previousOwner: "Bob"
+  };
+
+ for (let i = 0; i < product.length; i++) 
+ {
+     console.log(`${key}: ${product[key]}`)
+ }
+ 
+  delete product.previousOwner
+  product.doesItWork = true
+
+  if (product.price > 100) { 
+    product.discountPercentage = 10
+    
+    console.log(`Tenemos el ${product.name} en venta por $${product.price}, con el  ${product.discountPercentage}% de descuento!`);
+    
+    console.log("Precio sin porcentaje= " + product.price )
+   
+    totalPorcentaje = product.price * product.discountPercentage / 100
+    console.log("El monto del 10% es: " + totalPorcentaje)
+    product.price = product.price - totalPorcentaje;
+    console.log("El precio con el 10% de descuento es: "+ product.price);
+  }
